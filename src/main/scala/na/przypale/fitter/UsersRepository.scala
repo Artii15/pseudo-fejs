d946b81825d1954530e65e0fd34a1a8d522c93e9
Artii15 <artur.kowalski15@gmail.com>
2016-11-09 22:38:30 +0100

    Executing prepared statement

--- src/main/scala/na/przypale/fitter/UsersRepository.scala
+++ src/main/scala/na/przypale/fitter/UsersRepository.scala
@@ -5,7 +5,8 @@ import com.datastax.driver.core.Session
 class UsersRepository(val session: Session) {
 
   def insert(user: User) = {
-    session.prepare("INSERT INTO users(nick) VALUES(:nick)").bind()
-      .setString("nick", user.nick)
+    val statement = session.prepare("INSERT INTO users(nick) VALUES(:nick)").bind().setString("nick", user.nick)
+
+    session.execute(statement)
   }
 }
