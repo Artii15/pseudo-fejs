f9adf09fb8521415168b707dae96f79fb1106af0
Artii15 <artur.kowalski15@gmail.com>
2016-11-09 22:43:19 +0100

    Fetching user

--- src/main/scala/na/przypale/fitter/UsersRepository.scala
+++ src/main/scala/na/przypale/fitter/UsersRepository.scala
@@ -6,7 +6,11 @@ class UsersRepository(val session: Session) {
 
   def insert(user: User) = {
     val statement = session.prepare("INSERT INTO users(nick) VALUES(:nick)").bind().setString("nick", user.nick)
-
     session.execute(statement)
+  }
+
+  def getByNick(nick: String): User = {
+    val query = session.prepare("SELECT nick FROM users WHERE nick = :nick").bind().setString("nick", nick)
+    new User(session.execute(query).one().getString("nick"))
   }
 }
