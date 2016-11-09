1c508fd14abf8bf4c9a21b996f6fafbd56b2e69d
Artii15 <artur.kowalski15@gmail.com>
2016-11-09 22:54:16 +0100

    Structuring application

--- src/main/java/na/przypale/fitter/Main.java
+++ src/main/java/na/przypale/fitter/Main.java
@@ -1,5 +1,6 @@
 package na.przypale.fitter;
 
+import com.datastax.driver.core.Session;
 import na.przypale.fitter.repositories.UsersRepository;
 import na.przypale.fitter.repositories.cassandra.CassandraUsersRepository;
 import scala.runtime.BoxedUnit;
@@ -8,11 +9,15 @@ public class Main {
     public static void main(String[] args) {
         ClusterConnector.connect("127.0.0.1").apply(cluster -> {
             SessionConnector.connect(cluster).apply("test").apply(session -> {
-                UsersRepository usersRepository = new CassandraUsersRepository(session);
-                usersRepository.insert(new User("user1"));
+                testRepositories(session);
                 return BoxedUnit.UNIT;
             });
             return BoxedUnit.UNIT;
         });
+    }
+
+    public static void testRepositories(Session session) {
+        UsersRepository usersRepository = new CassandraUsersRepository(session);
+        usersRepository.insert(new User("user1"));
     }
 }
