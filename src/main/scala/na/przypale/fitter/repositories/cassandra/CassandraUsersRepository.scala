21d5b2227782e4bc76af20908c5c3bec71536628
Artii15 <artur.kowalski15@gmail.com>
2016-11-12 18:24:43 +0100

    Preparing statements only once

--- src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraUsersRepository.scala
+++ src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraUsersRepository.scala
@@ -6,13 +6,16 @@ import na.przypale.fitter.repositories.UsersRepository
 
 class CassandraUsersRepository(val session: Session) extends UsersRepository {
 
+  private val insertUserStatement = session.prepare("INSERT INTO users(nick) VALUES(:nick)")
+  private val getByNickStatement = session.prepare("SELECT nick FROM users WHERE nick = :nick")
+
   def insert(user: User) = {
-    val statement = session.prepare("INSERT INTO users(nick) VALUES(:nick)").bind().setString("nick", user.nick)
+    val statement = insertUserStatement.bind().setString("nick", user.nick)
     session.execute(statement)
   }
 
   def getByNick(nick: String): User = {
-    val query = session.prepare("SELECT nick FROM users WHERE nick = :nick").bind().setString("nick", nick)
-    new User(session.execute(query).one().getString("nick"))
+    val query = getByNickStatement.bind().setString("nick", nick)
+    User(session.execute(query).one().getString("nick"))
   }
 }
