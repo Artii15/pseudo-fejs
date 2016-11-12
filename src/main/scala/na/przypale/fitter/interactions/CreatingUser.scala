b0e60ac2fe1e0e97191a4a13e2ff888baff8dcff
Artii15 <artur.kowalski15@gmail.com>
2016-11-12 23:01:22 +0100

    exceptions handling

--- src/main/scala/na/przypale/fitter/interactions/CreatingUser.scala
+++ src/main/scala/na/przypale/fitter/interactions/CreatingUser.scala
@@ -3,11 +3,18 @@ package na.przypale.fitter.interactions
 import na.przypale.fitter.CommandLineReader
 import na.przypale.fitter.entities.User
 import na.przypale.fitter.repositories.UsersRepository
+import na.przypale.fitter.repositories.exceptions.UserAlreadyExistsException
 
 object CreatingUser {
   def create(usersRepository: UsersRepository): Unit = {
-    print("Provide nick: ")
-    usersRepository.insertUnique(User(CommandLineReader.readString()))
-    println("User successfully created")
+    try {
+      print("Provide nick: ")
+      usersRepository.insertUnique(User(CommandLineReader.readString()))
+      println("User successfully created")
+    }
+    catch {
+      case _: UserAlreadyExistsException => println("User with such nick already exists")
+      case _ => println("Unexpected error")
+    }
   }
 }
