3a2bc3cbbae5a7bf849e5f2a340ebbb05d0ad9aa
Artii15 <artur.kowalski15@gmail.com>
2016-11-13 22:50:18 +0100

    Handling logged user actions

--- src/main/scala/na/przypale/fitter/controls/LoggedUserControls.scala
+++ src/main/scala/na/przypale/fitter/controls/LoggedUserControls.scala
@@ -4,6 +4,8 @@ import na.przypale.fitter.entities.User
 import na.przypale.fitter.menu.{Action, ActionIntId, Menu}
 import na.przypale.fitter.repositories.UsersRepository
 
+import scala.annotation.tailrec
+
 class LoggedUserControls(val usersRepository: UsersRepository) {
   private val DELETE_ACTION_ID = 1
   private val LOGOUT_ACTION_ID = 2
@@ -13,8 +15,16 @@ class LoggedUserControls(val usersRepository: UsersRepository) {
     Action(ActionIntId(LOGOUT_ACTION_ID), s"$LOGOUT_ACTION_ID - Logout")
   ))
 
-  def interact(user: User): Unit = {
+  @tailrec
+  final def interact(user: User): Unit = {
     menu.display()
+    menu.read match {
+      case Some(action) => action.id match {
+        case ActionIntId(DELETE_ACTION_ID) =>
+        case ActionIntId(LOGOUT_ACTION_ID) =>
+      }
+      case _ => interact(user)
+    }
   }
 }
 
