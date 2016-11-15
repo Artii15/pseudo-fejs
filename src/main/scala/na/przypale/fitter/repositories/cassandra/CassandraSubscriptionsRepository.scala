f69fa67eab7bd2e1f3c87e73ebde136d0b1816d4
Artii15 <artur.kowalski15@gmail.com>
2016-11-15 20:34:27 +0100

    Creating subscriptions

--- src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraSubscriptionsRepository.scala
+++ src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraSubscriptionsRepository.scala
@@ -1,5 +1,7 @@
 package na.przypale.fitter.repositories.cassandra
 
+import java.util.stream.{Collectors, Stream}
+
 import com.datastax.driver.core.Session
 import na.przypale.fitter.entities.Subscription
 import na.przypale.fitter.repositories.SubscriptionsRepository
@@ -7,12 +9,13 @@ import na.przypale.fitter.repositories.SubscriptionsRepository
 class CassandraSubscriptionsRepository(session: Session) extends SubscriptionsRepository {
 
   private val createSubscriptionStatement = session.prepare(
-    "INSERT INTO subscriptions(subscriber, subscribed_person) VALUES(:subscriber, :subscribedPerson)")
+    "UPDATE subscriptions SET users = users + :subscribedPerson WHERE subscriber = :subscriber")
   def create(subscription: Subscription): Unit = {
     val Subscription(subscriber, subscribedPerson) = subscription
+
     val query = createSubscriptionStatement.bind()
       .setString("subscriber", subscriber)
-      .setString("subscribedPerson", subscribedPerson)
+      .setSet("subscribedPerson", Stream.of[String](subscribedPerson).collect(Collectors.toSet()))
 
     session.execute(query)
   }
