8e51680a65510342d581b7f4cd6491015a8dde10
Artii15 <artur.kowalski15@gmail.com>
2016-11-19 12:22:05 +0100

    Refactoring

--- src/main/scala/na/przypale/fitter/controls/PostsControls.scala
+++ src/main/scala/na/przypale/fitter/controls/PostsControls.scala
@@ -4,17 +4,15 @@ import na.przypale.fitter.menu.{Action, ActionIntId, Menu}
 
 class PostsControls(posts: Iterable[Post]) extends Controls {
 
-  private val EXIT_ACTION_ID = 1
-  private val MORE_ACTION_ID = 2
-  private val READ_ACTION_ID = 3
+  private val MORE_ACTION_ID = 1
+  private val READ_ACTION_ID = 2
+  private val EXIT_ACTION_ID = 3
 
-  val exitAction = Action(ActionIntId(EXIT_ACTION_ID), "Exit")
-  val menuActions = if(posts.isEmpty) List(exitAction) else List(
-    exitAction,
+  val menuActions = List(
     Action(ActionIntId(MORE_ACTION_ID), "More"),
-    Action(ActionIntId(READ_ACTION_ID), "Read post")
+    Action(ActionIntId(READ_ACTION_ID), "Read post"),
+    Action(ActionIntId(EXIT_ACTION_ID), "Exit")
   )
-
   val menu = new Menu(menuActions)
 
   override protected def getMenu: Menu = menu
@@ -25,7 +23,7 @@ class PostsControls(posts: Iterable[Post]) extends Controls {
   }
 
   override protected def closesControls(action: Action): Boolean = action.id match {
-    case ActionIntId(id) => id == EXIT_ACTION_ID
+    case ActionIntId(EXIT_ACTION_ID) => true
     case _ => false
   }
 }
--- src/main/scala/na/przypale/fitter/interactions/BrowsingPosts.scala
+++ src/main/scala/na/przypale/fitter/interactions/BrowsingPosts.scala
@@ -2,9 +2,8 @@ package na.przypale.fitter.interactions
 
 import java.text.SimpleDateFormat
 
-import na.przypale.fitter.CommandLineReader
 import na.przypale.fitter.controls.PostsControls
-import na.przypale.fitter.entities.{Post, Subscription, User}
+import na.przypale.fitter.entities.{Post, User}
 import na.przypale.fitter.repositories.{PostsRepository, SubscriptionsRepository}
 
 import scala.annotation.tailrec
@@ -12,39 +11,27 @@ import scala.annotation.tailrec
 class BrowsingPosts(postsRepository: PostsRepository, subscriptionsRepository: SubscriptionsRepository) {
 
   final def browse(user: User): Unit = {
-    val subscriptions = subscriptionsRepository.findSubscriptionsOf(user.nick)
-    val posts = findPosts(subscriptions)
-    posts.foreach(display)
-
-    val controls = new PostsControls()
-
-    if(posts.isEmpty) println("No posts to display")
-    else showMenu(posts, subscriptions)
-  }
-
-  def findPosts(subscriptions: Iterable[Subscription], lastShownPost: Option[Post] = None) = {
-    val subscribedPeopleNicks = subscriptions.map(subscription => subscription.subscribedPersonNick)
-    postsRepository.findByAuthors(subscribedPeopleNicks, lastShownPost)
-  }
-
-  val dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss")
-  private def display(post: Post): Unit = {
-    val Post(author, creationTime, content) = post
-    println(s"${dateFormat.format(creationTime)} $author:")
-    println(content)
+    val subscribedPeople = subscriptionsRepository.findSubscriptionsOf(user.nick)
+        .map(subscription => subscription.subscribedPersonNick)
+    searchPosts(subscribedPeople)
   }
 
   @tailrec
-  private def showMenu(posts: Iterable[Post], subscriptions: Iterable[Subscription]) {
-    println("1 - More")
-    println("2 - Read post")
-    println("3 - Exit")
+  private def searchPosts(subscribedPeople: Iterable[String], lastDisplayedPost: Option[Post] = None) {
+    val posts = postsRepository.findByAuthors(subscribedPeople, lastDisplayedPost)
+    posts.foreach(display)
 
-    CommandLineReader.readInt() match {
-      case 1 => showMenu(findPosts(subscriptions, posts.lastOption), subscriptions)
-      case 2 =>
-      case 3 =>
-      case _ => showMenu(posts, subscriptions)
+    if(posts.isEmpty) println("No more posts to display")
+    else {
+      new PostsControls(posts).interact()
+      searchPosts(subscribedPeople, lastDisplayedPost)
     }
+  }
+
+  val postDateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss")
+  private def display(post: Post): Unit = {
+    val Post(author, creationTime, content) = post
+    println(s"${postDateFormat.format(creationTime)} $author:")
+    println(content)
   }
 }
