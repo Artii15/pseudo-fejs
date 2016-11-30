d50bffed3b36cd428671250dd61eeb39f5671482
Kamil Gronowski <kamil540@gazeta.pl>
2016-11-30 20:40:48 +0100

    changed parent_id to string, fix comments display

--- src/main/scala/na/przypale/fitter/interactions/CreatingComment.scala
+++ src/main/scala/na/przypale/fitter/interactions/CreatingComment.scala
@@ -14,16 +14,12 @@ class CreatingComment(commentsRepository: CommentsRepository,
 
     userContent match {
       case comment: Comment =>
-        commentsRepository.create(Comment(comment.postAuthor, comment.postTimeId, UUIDs.timeBased(), user.nick, commentText, UUIDs.random(), comment.id))
+        commentsRepository.create(Comment(comment.postAuthor, comment.postTimeId, UUIDs.timeBased(), user.nick, commentText, UUIDs.random(), comment.id.toString))
         commentsCountersRepository.increaseAnswersCounter(comment)
-        //commentsCountersRepository.initiateCounters(comment)
-      //case Comment(postAuthor, postTimeId, commentTimeId, commentAuthor, content, id, parentId) =>
-        //commentsRepository.create(Comment(postAuthor, postTimeId, UUIDs.timeBased(), user.nick, commentText, UUIDs.random(), id))
-      case post: Post => //Post(author, timeId, content) =>
-        val comment = Comment(post.author, post.timeId, UUIDs.timeBased(), user.nick, commentText, UUIDs.random(), null)
+      case post: Post =>
+        val comment = Comment(post.author, post.timeId, UUIDs.timeBased(), user.nick, commentText, UUIDs.random(), "")
         commentsRepository.create(comment)
         postsCountersRepository.increaseCommentsCounter(post)
-        //commentsCountersRepository.initiateCounters(comment)
       case _ =>
     }
 
--- src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraCommentsRepository.scala
+++ src/main/scala/na/przypale/fitter/repositories/cassandra/CassandraCommentsRepository.scala
@@ -23,16 +23,21 @@ class CassandraCommentsRepository(session: Session) extends CommentsRepository {
       .setString("commentAuthor", commentAuthor)
       .setString("content", content)
       .setUUID("id", id)
-      .setUUID("parentId", parentId)
 
-    session.execute(insertCommentQuery)
+    val test = parentId match {
+        case null => insertCommentQuery
+        case _ => insertCommentQuery.setString("parentId", parentId)
+      }
+
+    session.execute(test)
   }
 
   private lazy val findByPostStatement = session.prepare(
     "SELECT post_author, post_time_id, comment_time_id, comment_author, content, id, parent_id " +
       "FROM comments " +
-      "WHERE post_author = :postAuthor AND post_time_id = :postTimeId AND comment_time_id > :timeId " +
-      s"LIMIT ${Config.DEFAULT_PAGE_SIZE}"
+      "WHERE post_author = :postAuthor AND post_time_id = :postTimeId AND comment_time_id > :timeId AND parent_id = ''" +
+      s"LIMIT ${Config.DEFAULT_PAGE_SIZE} " +
+      "ALLOW FILTERING"
   )
 
   override def findByPost(post: Post, lastCommentToSkip: Option[Comment]): Iterable[Comment] = {
@@ -46,7 +51,7 @@ class CassandraCommentsRepository(session: Session) extends CommentsRepository {
 
     JavaConverters.collectionAsScalaIterable(session.execute(query).all()).toVector
       .map(row => Comment(row.getString("post_author"), row.getUUID("post_time_id"), row.getUUID("comment_time_id"),
-        row.getString("comment_author"), row.getString("content"), row.getUUID("id"), row.getUUID("parent_id")))
+        row.getString("comment_author"), row.getString("content"), row.getUUID("id"), row.getString("parent_id")))
   }
 
 
@@ -65,12 +70,12 @@ class CassandraCommentsRepository(session: Session) extends CommentsRepository {
       .setString("postAuthor", comment.postAuthor)
       .setUUID("postTimeId", comment.postTimeId)
       .setUUID("timeId", timeId)
-      .setUUID("id", comment.id)
+      .setString("id", comment.id.toString)
     query.setFetchSize(Integer.MAX_VALUE)
 
     JavaConverters.collectionAsScalaIterable(session.execute(query).all()).toVector
       .map(row => Comment(row.getString("post_author"), row.getUUID("post_time_id"), row.getUUID("comment_time_id"),
-        row.getString("comment_author"), row.getString("content"), row.getUUID("id"), row.getUUID("parent_id")))
+        row.getString("comment_author"), row.getString("content"), row.getUUID("id"), row.getString("parent_id")))
   }
 
   def getTimeId(comment: Option[Comment]) = {
