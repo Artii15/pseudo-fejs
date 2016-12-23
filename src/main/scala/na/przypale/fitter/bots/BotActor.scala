24ab8e5deda9cd7a89ab9deb868f9017a4f28f83
Artii15 <artur.kowalski15@gmail.com>
2016-12-23 22:26:59 +0100

    Trying to register bot till success

--- src/main/scala/na/przypale/fitter/bots/BotActor.scala
+++ src/main/scala/na/przypale/fitter/bots/BotActor.scala
@@ -5,20 +5,31 @@ import java.util.UUID
 import akka.actor.Actor
 import na.przypale.fitter.bots.commands.Start
 import na.przypale.fitter.entities.Credentials
-import na.przypale.fitter.logic.CreatingUser
+import na.przypale.fitter.logic.{Authenticating, CreatingUser}
+import na.przypale.fitter.repositories.exceptions.UserAlreadyExistsException
 
-class BotActor(creatingUser: CreatingUser) extends Actor {
-  private val credentials = Credentials(UUID.randomUUID().toString, UUID.randomUUID().toString)
+import scala.annotation.tailrec
+
+class BotActor(creatingUser: CreatingUser, authenticating: Authenticating) extends Actor {
+  private var credentials: Credentials = generateCredentials()
 
   override def receive: Receive = {
     case Start => enterTheSystem()
   }
 
   private def enterTheSystem(): Unit = {
-
+    register()
   }
 
+  @tailrec
   private def register(): Unit = {
-    creatingUser.create(credentials)
+    try {
+      creatingUser.create(credentials)
+    }
+    catch {
+      case _: UserAlreadyExistsException => credentials = generateCredentials(); register()
+    }
   }
+
+  private def generateCredentials(): Credentials = Credentials(UUID.randomUUID().toString, UUID.randomUUID().toString)
 }
