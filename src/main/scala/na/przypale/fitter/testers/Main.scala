d173b1ab99aa6f4886771368781b618c3b117e5d
Artii15 <artur.kowalski15@gmail.com>
2016-12-28 19:43:29 +0100

    reading hosts from config

--- src/main/scala/na/przypale/fitter/testers/Main.scala
+++ src/main/scala/na/przypale/fitter/testers/Main.scala
@@ -6,11 +6,16 @@ import na.przypale.fitter.testers.actors.supervisors.UserActor
 import na.przypale.fitter.testers.commands.Start
 import na.przypale.fitter.testers.config.{SessionConfig, SystemConfig, UserActorConfig}
 
+import scala.collection.JavaConverters
+
 object Main {
 
   def main(args: Array[String]): Unit = {
     val config = ConfigFactory.load()
-    val systemConfig = new SystemConfig(config.getString("bots.systemName"), List("192.168.1.1"))
+    val systemConfig = new SystemConfig(
+      config.getString("bots.systemName"),
+      JavaConverters.collectionAsScalaIterable(config.getStringList("bots.hosts")),
+      config.getInt("akka.remote.netty.tcp.port"))
     val actorSystem = ActorSystem(systemConfig.actorSystemName)
 
     val sessionConfig = new SessionConfig("test")
