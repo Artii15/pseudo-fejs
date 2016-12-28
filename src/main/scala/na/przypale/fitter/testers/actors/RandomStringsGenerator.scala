cd5085b715dedc58d6fa9a2df99ec66afb62f1ea
Artii15 <artur.kowalski15@gmail.com>
2016-12-29 00:37:46 +0100

    Ensured repeating same set of nicks

--- src/main/scala/na/przypale/fitter/testers/actors/RandomStringsGenerator.scala
+++ src/main/scala/na/przypale/fitter/testers/actors/RandomStringsGenerator.scala
@@ -3,8 +3,10 @@ package na.przypale.fitter.testers.actors
 import java.util.UUID
 
 object RandomStringsGenerator {
-  def generateCyclic(cycleLength: Int): Stream[String] =
-    Stream.continually(generateRandomStrings(cycleLength)).flatten
+  def generateCyclic(cycleLength: Int): Stream[String] = {
+    val nicksToRepeat = generateRandomStrings(cycleLength)
+    Stream.continually(nicksToRepeat).flatten
+  }
 
   def generateRandomStrings(stringsCount: Int): Iterable[String] =
     Range.inclusive(1, stringsCount).map(_ => UUID.randomUUID().toString)
