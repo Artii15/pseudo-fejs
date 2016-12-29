40c5715718f0432151a6d9fdc680bb5bb14d28f6
Artii15 <artur.kowalski15@gmail.com>
2016-12-29 11:55:05 +0100

    Using seq instead of iterable

--- src/main/scala/na/przypale/fitter/testers/commands/registration/AccountsCreatingCommand.scala
+++ src/main/scala/na/przypale/fitter/testers/commands/registration/AccountsCreatingCommand.scala
@@ -2,4 +2,4 @@ package na.przypale.fitter.testers.commands.registration
 
 import na.przypale.fitter.testers.commands.nodes.TaskStart
 
-case class AccountsCreatingCommand(numberOfProcesses: Int, nicks: Iterable[String]) extends TaskStart
+case class AccountsCreatingCommand(numberOfProcesses: Int, nicks: Seq[String]) extends TaskStart
