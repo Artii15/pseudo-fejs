55ed7eb3ccfa9008d86cee434ae008b75b559226
artur <artur@DESKTOP-UH89CE1.home>
2016-12-29 10:55:52 +0100

    Waiting for user input removed

--- src/main/scala/na/przypale/fitter/testers/RemoteListener.scala
+++ src/main/scala/na/przypale/fitter/testers/RemoteListener.scala
@@ -8,8 +8,6 @@ import scala.io.StdIn
 object RemoteListener {
   def main(args: Array[String]): Unit = {
     val config = ConfigFactory.load()
-    val actorSystem = ActorSystem(config.getString("bots.systemName"))
-    StdIn.readLine()
-    actorSystem.terminate()
+    ActorSystem(config.getString("bots.systemName"))
   }
 }
