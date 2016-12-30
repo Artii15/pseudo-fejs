09c53bb27b5ca71ef1b61ed66d9bb0688dbdc509
Artii15 <artur.kowalski15@gmail.com>
2016-12-30 19:51:17 +0100

    Session owner abstract class

--- src/main/scala/fitter/testers/actors/leaders/SessionOwner.scala
+++ src/main/scala/fitter/testers/actors/leaders/SessionOwner.scala
@@ -1,5 +1,19 @@
 package fitter.testers.actors.leaders
 
-abstract class SessionOwner extends Leader {
+import com.datastax.driver.core.{Cluster, Session}
+import fitter.Dependencies
+import fitter.connectors.{ClusterConnector, SessionConnector}
+import fitter.testers.config.SessionConfig
 
+abstract class SessionOwner(sessionConfig: SessionConfig) extends Leader {
+
+  private val cluster: Cluster = ClusterConnector.makeConnection(sessionConfig.contactPoint)
+  private val session: Session = SessionConnector.makeSession(cluster, sessionConfig.keyspace)
+  protected val dependencies: Dependencies = new Dependencies(session)
+
+  override def postStop(): Unit = {
+    session.close()
+    cluster.close()
+    super.postStop()
+  }
 }
