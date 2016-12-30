701811f5c92496736a3b7678d54e11fff0f57f71
Artii15 <artur.kowalski15@gmail.com>
2016-12-30 20:55:59 +0100

    Account creating classes

--- src/main/scala/fitter/testers/actors/workers/RegistrationWorker.scala
+++ src/main/scala/fitter/testers/actors/workers/RegistrationWorker.scala
@@ -1,7 +1,10 @@
 package fitter.testers.actors.workers
 
 import fitter.testers.commands.registration.CreateAccount
+import fitter.testers.results.registration.AccountCreatingResult
 
-class RegistrationWorker extends Worker[CreateAccount, ] {
-  override protected def executeTask(taskStart: Nothing): Nothing = ???
+class RegistrationWorker extends Worker[CreateAccount, AccountCreatingResult] {
+  override protected def executeTask(taskStart: CreateAccount): AccountCreatingResult = {
+    AccountCreatingResult(None)
+  }
 }
---
+++ src/main/scala/fitter/testers/results/registration/AccountCreatingResult.scala
@@ -0,0 +1,5 @@
+package fitter.testers.results.registration
+
+import fitter.entities.Credentials
+
+case class AccountCreatingResult(createdAccountCredentials: Option[Credentials])
