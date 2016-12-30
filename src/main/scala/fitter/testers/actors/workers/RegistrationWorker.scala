d7f435f6f8dce5384d7451ee624cb35a9e148df9
Artii15 <artur.kowalski15@gmail.com>
2016-12-30 21:07:46 +0100

    Registration worker

--- src/main/scala/fitter/testers/actors/workers/RegistrationWorker.scala
+++ src/main/scala/fitter/testers/actors/workers/RegistrationWorker.scala
@@ -1,10 +1,23 @@
 package fitter.testers.actors.workers
 
+import java.util.UUID
+
+import fitter.entities.Credentials
+import fitter.logic.CreatingUser
+import fitter.repositories.exceptions.{UserAlreadyExistsException, UserNotExistsException}
 import fitter.testers.commands.registration.CreateAccount
 import fitter.testers.results.registration.AccountCreatingResult
 
-class RegistrationWorker extends Worker[CreateAccount, AccountCreatingResult] {
-  override protected def executeTask(taskStart: CreateAccount): AccountCreatingResult = {
-    AccountCreatingResult(None)
+class RegistrationWorker(creatingUser: CreatingUser) extends Worker[CreateAccount, AccountCreatingResult] {
+  override protected def executeTask(task: CreateAccount): AccountCreatingResult = {
+    val credentials = Credentials(task.nick, UUID.randomUUID().toString)
+    try {
+      creatingUser.create(credentials)
+      AccountCreatingResult(Some(credentials))
+    }
+    catch {
+      case _: UserAlreadyExistsException | _: UserNotExistsException => AccountCreatingResult(None)
+    }
+
   }
 }
--- src/main/scala/fitter/testers/commands/registration/CreateAccount.scala
+++ src/main/scala/fitter/testers/commands/registration/CreateAccount.scala
@@ -2,4 +2,4 @@ package fitter.testers.commands.registration
 
 import fitter.testers.commands.nodes.TaskStart
 
-class CreateAccount extends TaskStart
+class CreateAccount(val nick: String) extends TaskStart
