2a456b48ef16166cc6c6ade7ffc4e751371ea6d3
Artii15 <artur.kowalski15@gmail.com>
2016-12-31 11:21:51 +0100

    Exceptions handling fixed

--- src/main/scala/fitter/testers/actors/workers/EventWorker.scala
+++ src/main/scala/fitter/testers/actors/workers/EventWorker.scala
@@ -1,6 +1,6 @@
 package fitter.testers.actors.workers
 
-import fitter.entities.{Credentials, Event}
+import fitter.entities.Credentials
 import fitter.logic.CreatingUser
 import fitter.repositories.cassandra.CassandraEventsRepository
 import fitter.repositories.exceptions.{EventParticipantLimitExceedException, UserAlreadyExistsException, UserNotExistsException}
@@ -12,23 +12,15 @@ class EventWorker(eventsRepository: CassandraEventsRepository, creatingUser: Cre
   extends Worker[JoinEvent, JoinedParticipant] {
 
   override protected def executeTask(task: JoinEvent): JoinedParticipant = {
-    val credentials = makeAccount()
-    JoinedParticipant(joinEvent(credentials, task.event))
-  }
-
-  private def makeAccount(): Credentials = {
-    val credentials = Credentials(RandomStringsGenerator.generateRandomString(), RandomStringsGenerator.generateRandomString())
-    creatingUser.create(credentials)
-    credentials
-  }
-
-  private def joinEvent(credentials: Credentials, event: Event): Option[Credentials] = {
     try {
-      eventsRepository.assignUserToEvent(event, credentials.nick)
-      Some(credentials)
+      val credentials = Credentials(RandomStringsGenerator.generateRandomString(), RandomStringsGenerator.generateRandomString())
+      creatingUser.create(credentials)
+      eventsRepository.assignUserToEvent(task.event, credentials.nick)
+      JoinedParticipant(Some(credentials))
     }
     catch {
-      case _: EventParticipantLimitExceedException | _: UserNotExistsException | _: UserAlreadyExistsException => None
+      case _: EventParticipantLimitExceedException | _: UserNotExistsException | _: UserAlreadyExistsException =>
+        JoinedParticipant(None)
     }
   }
 }
